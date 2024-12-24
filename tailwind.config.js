9921910119c0e241fb25eeedf1cbda76922bb288
gauravpawar47 <gauravpawar883@gmail.com>
2024-12-24 16:46:46 +0530

    INITIAl v1.0

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // This will ensure that Tailwind processes JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
