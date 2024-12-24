62aa01c54a4fc450151de6a2f75641204c949817
gauravpawar47 <gauravpawar883@gmail.com>
2024-12-24 19:08:15 +0530

    STYLING v1.1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../App.css'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-indigo-900 text-white py-2'>
+    <nav className='flex justify-between bg-black text-white py-2'>
         <div className="logo">
-            <span className='font-bold text-xl mx-8'>iTask</span>
+            <span className='font-bold text-xl mx-8'>To Do</span>
         </div>
       <ul className="flex gap-8 mx-9">
-        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-        <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Log In</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Sign In</li>
       </ul>
     </nav>
   )
